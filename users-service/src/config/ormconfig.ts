25c1d5f8460cc6f558e309c2f8cc68aa3541ea23
btsuro <benjamin.tsuro@icecash.co.zw>
2024-06-10 17:43:07 +0200

    init

--- users-service/src/config/ormconfig.ts
+++ users-service/src/config/ormconfig.ts
@@ -1,15 +1,16 @@
 import { DataSourceOptions } from 'typeorm';
-import { Users } from '../entities/user/user.entity';
+import * as dotenv from 'dotenv'
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const connectionOptions: DataSourceOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'test',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   charset: 'utf8',
   synchronize: false,
-  logging: true,
+  logging: process.env.NODE_ENV !== 'production',
   entities:[
     './**/*.entity.{ts,js}',
   ],
--- users-service/src/index.ts
+++ users-service/src/index.ts
@@ -5,15 +5,11 @@ import { myDataSource } from './app-data-source';
 const PORT = process.env.PORT || 5001;
 
 const main = async () => {
-
   myDataSource.initialize().then(() => {
-
     logger.info('database connection created');
-
     app.listen(PORT, () => {
       logger.info(`Server running at ${PORT}`);
     });
-
   }).catch((err) => {
     console.error('Error during Data Source initialization:', err);
   });
